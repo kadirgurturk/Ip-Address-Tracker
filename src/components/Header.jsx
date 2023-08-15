2fedaef063f5c8cc4babee9c58f14ec66e51f91f
kadirgurturk <89667179+kadirgurturk@users.noreply.github.com>
2023-08-16 00:10:23 +0300

    header start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,34 @@
 import React from 'react'
 import logo1 from "../assets/pattern-bg-desktop.png"
+import arrow from "../assets/arrow.svg";
 
 export default function Header() {
   return (
     <div className='header' style={{backgroundImage:`url(${logo1})`}}>
-        <h2>IP Address Tracker</h2>
-        <div className="header-input">
-            <input type="text" />
-            <button></button>
-        </div>
+        <div className="header-container">
+            <h2>IP Address Tracker</h2>
+            <div className="header-input">
+                <input type="text" />
+                <button><img src={arrow} alt="arrow"/></button>
+            </div>
 
-        <div className="header-info">
-            <div className="address header-info-items"> 
-                <p className="info-header"></p>
-                <p className="info-content"></p>
-            </div>
-            <div className="location header-info-items">
-                <p className="info-header"></p>
-                 <p className="info-content"></p>
-            </div>
-            <div className="timezone header-info-items">
-                <p className="info-header"></p>
-                <p className="info-content"></p>
-            </div>
-            <div className="ısp header-info-items">     
-                <p className="info-header"></p>
-                <p className="info-content"></p>
+            <div className="header-info">
+                <div className="address header-info-items"> 
+                    <p className="info-header"></p>
+                    <p className="info-content"></p>
+                </div>
+                <div className="location header-info-items">
+                    <p className="info-header"></p>
+                     <p className="info-content"></p>
+                </div>
+                <div className="timezone header-info-items">
+                    <p className="info-header"></p>
+                    <p className="info-content"></p>
+                </div>
+                <div className="ısp header-info-items">     
+                    <p className="info-header"></p>
+                    <p className="info-content"></p>
+                </div>
             </div>
         </div>
     </div>
